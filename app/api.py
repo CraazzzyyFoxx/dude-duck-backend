b01c15be1bfe72ff4fbe8f8dd1507c68fa93d6cd
CraazzzyyFoxx <andrey.pod19@gmail.com>
2023-09-11 12:32:03 +0300

    v2 pre-release

--- app/api.py
+++ app/api.py
@@ -14,7 +14,7 @@ from app.services.settings.views import router as settings_router
 router = APIRouter()
 router.include_router(auth_router)
 
-api_router = APIRouter(prefix="/api/v1")
+api_router = APIRouter()
 api_router.include_router(settings_router)
 api_router.include_router(users_router)
 api_router.include_router(orders_router)
