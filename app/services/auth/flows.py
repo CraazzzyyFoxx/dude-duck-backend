b0c7f6b814f782b704d2604646f747bd724efa26
CrazzzyyFoxx <craazzzyyfoxx@gmail.com>
2023-09-25 10:30:41 +0300

    Bug fix auth

--- app/services/auth/flows.py
+++ app/services/auth/flows.py
@@ -71,23 +71,14 @@ async def get_current_user(
     active: bool = False,
     verified: bool = False,
     superuser: bool = False,
+    api: bool = False,
 ):
     user: models.User | None = None
     if token is not None:
-        user = await service.read_token(token)
-    verify_user(user, active, verified, superuser)
-    return user, token
-
-
-async def get_current_user_api(
-    token: str,
-    active: bool = False,
-    verified: bool = False,
-    superuser: bool = False,
-):
-    user: models.User | None = None
-    if token is not None:
-        user = await service.read_token(token)
+        if api:
+            user = await service.read_token_api(token)
+        else:
+            user = await service.read_token(token)
     verify_user(user, active, verified, superuser)
     return user, token
 
@@ -96,6 +87,7 @@ def current_user(
     active: bool = False,
     verified: bool = False,
     superuser: bool = False,
+    api: bool = False
 ):
     async def current_user_dependency(token: Annotated[str, Depends(oauth2_scheme)]):
         user, _ = await get_current_user(
@@ -103,21 +95,9 @@ def current_user(
             active=active,
             verified=verified,
             superuser=superuser,
+            api=api
         )
         return user
-
-    return current_user_dependency
-
-
-def current_user_token(active: bool = True, verified: bool = False, superuser: bool = False):
-    async def current_user_dependency(token: Annotated[str, Depends(oauth2_scheme)]):
-        user, token = await get_current_user(
-            token,
-            active=active,
-            verified=verified,
-            superuser=superuser,
-        )
-        return user, token
 
     return current_user_dependency
 
@@ -135,6 +115,7 @@ def current_user_api(
             active=active,
             verified=verified,
             superuser=superuser,
+            api=True
         )
         return user
 
@@ -144,7 +125,7 @@ def current_user_api(
 current_active = current_user(active=True)
 current_active_superuser = current_user(active=True, superuser=True)
 current_active_verified = current_user(active=True, verified=True)
-current_active_superuser_api = current_user(active=True, superuser=True)
+current_active_superuser_api = current_user_api(active=True, superuser=True)
 
 
 async def resolve_user(
